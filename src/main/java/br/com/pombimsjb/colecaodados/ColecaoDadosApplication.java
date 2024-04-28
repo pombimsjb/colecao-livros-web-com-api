368e02ec7eb42ecd772a49465c27ee0e815d7073
pombimsjb <itachisolid@gmail.com>
2024-04-27 22:13:19 -0300

    Criação do Projeto

---
+++ src/main/java/br/com/pombimsjb/colecaodados/ColecaoDadosApplication.java
@@ -0,0 +1,13 @@
+package br.com.pombimsjb.colecaodados;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ColecaoDadosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ColecaoDadosApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/pombimsjb/colecaodados/ColecaoDadosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.pombimsjb.colecaodados;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ColecaoDadosApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
